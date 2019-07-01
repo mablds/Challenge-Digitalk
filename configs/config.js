9ec13ba20d20792bf368a50b690c66fcc1215ef7
Marcelo Arthur <marcelobragalemos@gmail.com>
2019-07-01 19:42:56 -0300

    controllers created

--- configs/config.js
+++ configs/config.js
@@ -1,11 +1,14 @@
 const express = require('express')
 
+//módulo de exportação das configurações para o server Express (app).
 module.exports.setUpServer = () => {
     const app = express()
     const bodyParser = require('body-parser')
 
     app.use(bodyParser.json())
     app.use(bodyParser.urlencoded({ extended: true }))
+
+    app.use(require('../controllers'))
 
     return app
 }
