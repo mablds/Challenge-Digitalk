6a0df70892cdc80f5e3153a23666194906bdc0b4
Marcelo Arthur <marcelobragalemos@gmail.com>
2019-07-02 10:10:46 -0300

    useful guide referenced on the usersDAO to study

---
+++ model/usersDAO.js
@@ -0,0 +1,3 @@
+// https://blog.rocketseat.com.br/nodejs-express-sequelize/
+// https://blog.logrocket.com/setting-up-a-restful-api-with-node-js-and-postgresql-d96d6fc892d8/
+// http://docs.sequelizejs.com/manual/getting-started
