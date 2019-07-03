0bdfe3efc1e6b05c83c65863786b87e95ef2ab64
Marcelo Arthur <marcelobragalemos@gmail.com>
2019-07-03 16:17:34 -0300

    usersDAO structured

--- model/usersDAO.js
+++ model/usersDAO.js
@@ -1,3 +1,35 @@
-// https://blog.rocketseat.com.br/nodejs-express-sequelize/
-// https://blog.logrocket.com/setting-up-a-restful-api-with-node-js-and-postgresql-d96d6fc892d8/
-// http://docs.sequelizejs.com/manual/getting-started
+const Sequelize = require('sequelize')
+const Model = Sequelize.Model;
+
+class User extends Model {}
+
+module.exports = (sequelize) => {
+    return User.init({
+        // attributes
+        name: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        username: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        email: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        password: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        admin: {
+            type: Sequelize.BOOLEAN,
+            allowNull: false
+        },
+    }, {
+        sequelize,
+        modelName: 'user'
+            // options
+    });
+
+}
