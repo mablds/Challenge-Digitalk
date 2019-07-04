b51e04e1e1a84b2e66ffea5470dbeb4de721aed7
Marcelo Arthur <marcelobragalemos@gmail.com>
2019-07-04 15:16:14 -0300

    user handler routes

--- controllers/users/usersHandler.js
+++ controllers/users/usersHandler.js
@@ -1,41 +1,17 @@
 const express = require('express'),
     router = express.Router()
-
-const Sequelize = require('sequelize')
-const sequelize = new Sequelize('postgresql://uzm2w6azfyvlygl5keka:5xpBBY6yzxXcJtPtBGfY@bsqufuog04qvezb8p8zq-postgresql.services.clever-cloud.com:5432/bsqufuog04qvezb8p8zq', { dialect: 'postgres' })
-
-const User = require('../../model/DAO/usersDAO')(sequelize)
-
-const authPostgre = async() => {
-        try {
-            await sequelize.authenticate()
-            await sequelize.sync()
-            console.log('Connection has been established successfully.')
-        } catch (err) {
-            console.error('Unable to connect to the database:', err)
-        }
-    }
     //Rotas para a realização do CRUD dos usuários:
 
-//Consulta usuários
-router.post('/', async(req, res) => {
-    try {
-        await authPostgre()
-        await User.sync({ force: true }) // Now the `users` table in the database corresponds to the model definition
-        console.log('Sync - Ok. Models defined.')
-    } catch (err) {
-        console.log(err)
-    }
+const createUser = require('./createUser')
+const getUsers = require('./getUsers')
+    //Consulta usuários
 
-})
+//botar um middleware de token válido como adm
+router.post('/', createUser)
 
-//Edita usuários
-router.put('/:id', (req, res) => {
-    res.send('PUT realizado com sucesso em /users')
-})
+router.get('/', getUsers.findAll)
 
-//Apaga usuários
-router.delete('/:id', (req, res) => {
-    res.send('DELETE realizado com sucesso em /users')
-})
+router.get('/:id', getUsers.byId)
+
+
 exports.router = router
