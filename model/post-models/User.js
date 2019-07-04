2bbad75f87f5ba6a7a4d9bd136db75490ff405f1
Marcelo Arthur <marcelobragalemos@gmail.com>
2019-07-04 15:16:01 -0300

    user model

---
+++ model/post-models/User.js
@@ -0,0 +1,32 @@
+const Sequelize = require('sequelize')
+const Model = Sequelize.Model
+
+class User extends Model {}
+
+module.exports = (sequelize) => {
+    return User.init({
+        name: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        username: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        email: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        password: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        admin: {
+            type: Sequelize.BOOLEAN,
+            allowNull: false
+        },
+    }, {
+        sequelize,
+        modelName: 'user'
+    })
+}
