26433e4338b460bb33838cc1e0951cdb33566b40
Marcelo Arthur <marcelobragalemos@gmail.com>
2019-07-05 16:18:58 -0300

    User ID bug fixed

--- controllers/users/deleteUser.js
+++ controllers/users/deleteUser.js
@@ -2,7 +2,7 @@ const User = require('../../model/DAO/usersDAO')
 
 module.exports = async(req, res) => {
     try {
-        await User.delete(req)
+        await User.delete(req.params.id)
         res.status(201).send("User deleted.")
     } catch {
         res.status(500).send('Server Error')
