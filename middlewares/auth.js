3fea378e104fa4864dda462760ab96d3e9dabc23
Marcelo Arthur <marcelobragalemos@gmail.com>
2019-07-08 13:35:08 -0300

    if !token = return bad request

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,28 +4,24 @@ const hash = authConfig.secret
 
 module.exports = (req, res, next) => {
     let token = req.headers['x-access-token'] || req.headers['authorization']
+
+    if (!token) return res.status(401).send('Token de autenticação não informado. Por favor realizar a autenticação através do método POST na rota /auth')
+
     if (token.startsWith('Bearer ')) {
         // Tirando da string a palavra 'Bearer' caso ela esteja no início da string
         token = token.slice(7, token.length)
     }
 
-    if (token) {
-        jwt.verify(token, hash, (err, decoded) => {
-            if (err) {
-                return res.json({
-                    success: false,
-                    message: 'Token informado não é válido.'
-                })
-            } else {
-                req.decoded = decoded
-                console.log(req.decoded)
-                next()
-            }
-        })
-    } else {
-        return res.json({
-            success: false,
-            message: 'Token de autenticação não informado. Por favor realizar a autenticação através do método POST na rota /auth'
-        })
-    }
+    jwt.verify(token, hash, (err, decoded) => {
+        if (err) {
+            return res.json({
+                success: false,
+                message: 'Token informado não é válido.'
+            })
+        } else {
+            req.decoded = decoded
+            console.log(req.decoded)
+            next()
+        }
+    })
 }
