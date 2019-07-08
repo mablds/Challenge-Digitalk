cf5850f7db1227ae2a820ad59471ad01e41dc52a
Marcelo Arthur <marcelobragalemos@gmail.com>
2019-07-08 16:06:12 -0300

    report by range on headers

--- controllers/bills/report/getReport.js
+++ controllers/bills/report/getReport.js
@@ -1,17 +1,24 @@
+const Range = require('date-range-array')
 const Receber = require('../../../model/DAO/incomingDAO')
 const Pagar = require('../../../model/DAO/outgoingDAO')
 
 module.exports.FullReport = async(req, res) => {
+    const startDate = req.headers['init-date']
+    const endDate = req.headers['end-date']
+
+
     const receberBD = await Receber.findAll()
     const pagarBD = await Pagar.findAll()
 
-    const fullReport = {
-        pagar: pagarBD.map(e => e.dataValues),
-        receber: receberBD.map(e => e.dataValues)
-    }
+    const startD = new Date(startDate)
+    const endD = new Date(endDate)
+    if (startD > endD) return res.status(400).send('A data de início inserida no headers é maior que a data final.')
 
-    const aPagar = fullReport.pagar
-    const aReceber = fullReport.receber
+    const dateRange = startDate && endDate ? Range(startDate, endDate) : undefined
+
+    const aPagar = parsedValues(dateRange, pagarBD)
+
+    const aReceber = parsedValues(dateRange, receberBD)
 
     const reduceFunction = (acc, e) => acc + e.value
 
@@ -40,3 +47,21 @@ module.exports.FullReport = async(req, res) => {
         totalAReceber,
     })
 }
+
+const parsedValues = (range, value) => {
+    if (range) {
+        return value
+            .map(e => e.dataValues)
+            .filter(e => {
+                if (e.paymentDate) {
+                    const dt = new Date(e.paymentDate)
+                    const formatedDate = dt.getFullYear() + '-' + addZero(dt.getMonth() + 1) + '-' + addZero(dt.getDate())
+                    if (range.includes(formatedDate))
+                        return true
+                }
+            })
+    } else
+        return value.map(e => e.dataValues)
+}
+
+const addZero = (n) => n < 10 ? '0' + n : n
